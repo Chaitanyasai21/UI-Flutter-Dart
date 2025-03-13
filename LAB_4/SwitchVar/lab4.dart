bc0d0a86e7c0f62da9b0e2e848264e8b2f3a628d
Chaitanyasai21 <chaitanyasai.juluru@gmail.com>
2025-03-13 12:25:00 +0530

    Update lab4.dart

--- LAB_4/SwitchVar/lab4.dart
+++ LAB_4/SwitchVar/lab4.dart
@@ -1,3 +1,11 @@
+import 'package:lab4/lab4.dart' ;
+import 'dart:io';
+
+void main(List<String> arguments) {
+	var x = stdin.readLineSync()!;  
+	lab4 lb = lab4();  
+ 	print(lb.sname(x));
+}
 class lab4{
 	String sname(String color){
 		return(switch(color){
